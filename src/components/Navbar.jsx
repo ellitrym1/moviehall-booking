1d2224365b16f750bfb298dc33cf8c4ad3a2ef89
Aditya Ratna Shakya <ellitrym1@gmail.com>
2022-06-08 18:48:36 +0100

    Added css for navabar and styled the brand part

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import "./Navbar.css";
 
 export const Navbar = () => {
-    return <div>Navbar</div>;
+    return (
+        <div className="navbar">
+            <div className="brand">
+                <img src="" alt="logo" className="logo" />
+                <h3>MOVIE HALL</h3>
+            </div>
+
+            <ul className="nav-links">
+                <li>Movies</li>
+                <li>Snacks</li>
+                <li>Support</li>
+            </ul>
+        </div>
+    );
 };
